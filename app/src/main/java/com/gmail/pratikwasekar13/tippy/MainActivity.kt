945f0917df773c4fd7cbc0214a4c08283532519f
Pratik <pratikwasekar13@gmail.com>
2023-09-21 20:24:56 +0530

    My first android app!!

---
+++ app/src/main/java/com/gmail/pratikwasekar13/tippy/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.gmail.pratikwasekar13.tippy
+
+import android.animation.ArgbEvaluator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import java.time.temporal.TemporalAmount
+
+private const val TAG = "MainActivity"
+private const val INITIAL_TIP_PERCENT = 15
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var etBaseAmount: EditText
+    private lateinit var SeekBarTip: SeekBar
+    private lateinit var tvTipPercentLabel: TextView
+    private lateinit var tvTipAmount: TextView
+    private lateinit var tvTotalAmount: TextView
+    private lateinit var tvTipDescription: TextView
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        etBaseAmount = findViewById(R.id.etBaseAmount)
+        SeekBarTip = findViewById(R.id.seekBarTip)
+        tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
+        tvTipAmount = findViewById(R.id.tvTipAmount)
+        tvTotalAmount = findViewById(R.id.tvTotalAmount)
+        tvTipDescription = findViewById(R.id.tvTipDescription)
+
+
+        SeekBarTip.progress = INITIAL_TIP_PERCENT
+        tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
+        updateTipDescription(INITIAL_TIP_PERCENT)
+
+        SeekBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                Log.i(TAG, "onProgressChanged $p1")
+                tvTipPercentLabel.text = "$p1%"
+                computeTipAndTotal()
+                updateTipDescription(p1)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+        etBaseAmount.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i(TAG, "afterTextChanged $p0")
+                computeTipAndTotal()
+            }
+
+        })
+
+    }
+
+    private fun updateTipDescription(tipPercent: Int) {
+        val tipDescription = when (tipPercent){
+            in 0..9 -> "Poor"
+            in 1..14 -> "Acceptable"
+            in 15..19 ->  "Good"
+            in 20..24 -> "Great"
+            else -> "Amazing"}
+        tvTipDescription.text = tipDescription
+        //update the color based on the tip percent
+        val color = ArgbEvaluator().evaluate(
+            tipPercent.toFloat() / SeekBarTip.max,
+            ContextCompat.getColor(this, R.color.color_worst_tip),
+            ContextCompat.getColor(this, R.color.color_best_tip)
+        )as Int
+        tvTipDescription.setTextColor(color)
+    }
+
+    private fun computeTipAndTotal() {
+        if(etBaseAmount.text.isEmpty()){
+            tvTipAmount.text = ""
+            tvTotalAmount.text = ""
+            return
+        }
+        // 1. Get the value of the Base amount and tip percent
+        val BaseAmount = etBaseAmount.text.toString().toDouble()
+        val tipPercent = SeekBarTip.progress
+        // 2. Calculate the tip percent and the total amount
+        val tipAmount = BaseAmount * tipPercent/100
+        val totalAmount = tipAmount + BaseAmount
+        // 3. update the UI
+       // tvTipAmount.text = tipAmount.toString()
+        tvTipAmount.text = "%.2f".format(tipAmount)
+
+       // tvTotalAmount.text = totalAmount.toString() ''this was giving a big decimal value''
+        tvTotalAmount.text = "%.2f".format(totalAmount)
+    }
+}
